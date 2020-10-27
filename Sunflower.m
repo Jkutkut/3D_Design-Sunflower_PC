ce3f32085b6218fc528f2b4347cff97c56e83255
jkutkut <jorgeregonzalez@gmail.com>
2020-10-27 09:34:44 +0100

    made it

---
+++ Sunflower.m
@@ -0,0 +1,16 @@
+l = 1;
+s = 0.001;
+r = sunflower(l, s);
+scatter(real(r),imag(r));
+
+
+function [r] = sunflower(l, s)
+%     r = zeros(l / s);
+    r = [];
+    PHI = 137.5077640500378546463487;
+    phi = 0;
+    for a=0:s:l
+        phi=phi + PHI;
+        r(end + 1) = a * exp(1j * phi);
+    end
+end
